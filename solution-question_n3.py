cc4f695fe686e842f00c5e5754aac5881ad141f5
Abdoul Djalil <abdouldjalilo@gmail.com>
2024-03-18 16:23:12 +0100

    lit change

--- solution-question_n3.py
+++ solution-question_n3.py
@@ -1,22 +1,17 @@
 import random
 
-# Fonction pour simuler un lancer de dé
 def lancer_de():
     return random.randint(1, 6)
 
-# Nombre total de lancers
-total_lancers = 10000
 
-# Nombre de lancers dans le sous-échantillon
+total_lancers = 10000 
 lancers_sous_echantillon = 1000
-
-# Initialisation des compteurs pour les résultats
 frequence_6_total = 0
 frequence_4_sous_echantillon = 0
 
-# Répéter l'expérience 5 fois
+
 for _ in range(5):
-    # Réinitialiser le compteur pour le sous-échantillon
+
     frequence_4_sous_echantillon_temp = 0
     
     # Effectuer 10 000 lancers
@@ -31,13 +26,13 @@ for _ in range(5):
         if lancer_de() == 4:
             frequence_4_sous_echantillon_temp += 1
     
-    # Calculer la moyenne des fréquences de 4 dans le sous-échantillon
+    # la moyenne des fréquences de 4 dans le sous-échantillon
     frequence_4_sous_echantillon += frequence_4_sous_echantillon_temp / lancers_sous_echantillon
 
-# Calculer les moyennes des fréquences de 6 pour les 10 000 lancers
+# les moyennes des fréquences de 6 pour les 10 000 lancers
 m = frequence_6_total / (total_lancers * 5)
 
-# Calculer les moyennes des fréquences de 4 pour les 1 000 lancers dans le sous-échantillon
+# les moyennes des fréquences de 4 pour les 1 000 lancers dans le sous-échantillon
 n = frequence_4_sous_echantillon / 5
 
 print("m :", m)
